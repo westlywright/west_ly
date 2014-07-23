b96d6a56ad332589eb5d92a956e6b0677d8f730e
Westly Wright <westly.wright@zefr.com>
2014-07-22 22:00:00 -0700

    Added console easter egg.

--- src/app/app-config.js
+++ src/app/app-config.js
@@ -19,5 +19,17 @@ mainApp.config([
             url: '/',
             templateUrl: 'src/app/views/home.html'
         });
+
+        // Utilities functions below
+        // Easter Egg
+        function isInspectOpen()
+        {
+            console.profile();
+            console.profileEnd();
+            if (console.clear) console.clear();
+            console.log('Whoa, Whoa, Whoa. Me thinks you\'re a developer or someone who wants to see some of my code. You chould click the link for Github up there (don\'t worry, this project is hosted there as well). Better yet...if you\'d like to talk email me westly.wright@gmail.com or call me at (928)458-5429.');
+            return;
+        }
+        isInspectOpen();
     }
 ]);
